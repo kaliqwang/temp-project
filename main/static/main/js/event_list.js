35ef11e11c0c283638f5e51a2d126799f56353bd
Rayner Kristanto <kristanto.rayner2015@gmail.com>
2016-07-18 17:09:18 -0400

    fixed event-list.js names

--- main/static/main/js/event_list.js
+++ main/static/main/js/event_list.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
     var $events = $('#eventsString');
-    var $eventsArray = $events.html().split(";,");
+    var eventsArray = $events.html().split(";,");
     var $eventsList = $('#events-list');
-    var $firstEvent = $eventsArray[0];
-    var $lastEvent = $eventsArray[$eventsArray.length - 1];
-    var $date = $firstEvent.substring($firstEvent.length - 14, $firstEvent.length - 3);
-    var $month = parseInt($date.trim().substring(0, 2), 10);
-    var $monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var firstEvent = eventsArray[0];
+    var lastEvent = eventsArray[eventsArray.length - 1];
+    var currentDate = firstEvent.substring(firstEvent.length - 14, firstEvent.length - 3);
+    var currentMonth = parseInt(currentDate.trim().substring(0, 2), 10);
+    var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     //Adjusts for extra chars on first and last event
-    $eventsArray[0] = $firstEvent.substring(1);
-    $eventsArray[$eventsArray.length - 1] = $lastEvent.substring(0, $lastEvent.length - 3);
+    eventsArray[0] = firstEvent.substring(1);
+    eventsArray[eventsArray.length - 1] = lastEvent.substring(0, lastEvent.length - 3);
 
     //Appends the first event's month and date
-    $eventsList.append("<h2>" + $monthArray[$month - 1] + "</h2>");
-    $eventsList.append("<h5 style = 'color: #2e78ba'>" + $date + "</h5>");
+    $eventsList.append("<h2>" + monthArray[currentMonth - 1] + "</h2>");
+    $eventsList.append("<h5 style = 'color: #2e78ba'>" + currentDate + "</h5>");
     //If month of event is different, appends the new month
     //If date of event is different, appends the new date
     //Appends the event no matter what
-    $eventsArray.forEach(function(event) {
+    eventsArray.forEach(function(event) {
         var date = event.substring(event.length - 14, event.length - 3);
         var month = parseInt(date.trim().substring(0, 2), 10);
-        if (date != $date) {
-            if (month != $month) {
-                $eventsList.append("<h2>" + $monthArray[month - 1] + "</h2>");
-                $month = month;
+        if (date != currentDate) {
+            if (month != currentMonth) {
+                $eventsList.append("<h2>" + monthArray[month - 1] + "</h2>");
+                currentMonth = month;
             }
             $eventsList.append("<h5 style = 'color: #2e78ba'>" + date + "</h5>");
-            $date = date;
+            currentDate = date;
         }
         $eventsList.append("<li>" + event.substring(11, event.length - 14) + "</li>");
     });
