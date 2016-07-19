bf59de2515ea3adb4dfd7d6c88c3a9d029fa030f
kaliqwang <kaliqwang@yahoo.com>
2016-07-18 21:50:41 -0400

    removed line of code from event list js

--- main/static/main/js/event_list.js
+++ main/static/main/js/event_list.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     // Helper function for adding suffixes to dates
     function nth(d) {
-        if(d>3 && d<21) return 'th';
         switch (d % 10) {
             case 1:  return "<sup>st</sup>";
             case 2:  return "<sup>nd</sup>";
